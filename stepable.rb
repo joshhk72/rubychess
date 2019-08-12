9a7bacf8f5a83693fada6e1ed7e8176ca9436a86
joshhk72 <joshhk72@gmail.com>
2019-08-12 18:00:41 -0400

    Discover issue with stepable units (king, knight) not being able to move

--- stepable.rb
+++ stepable.rb
@@ -2,8 +2,9 @@ module Stepable
 
   def moves
     potential_moves = move_diffs.map! { |diff| [diff[0] + pos[0], diff[1] + pos[1]] }
-    potential_moves.select! { |move| board.valid_pos?([move[0], move[1]]) }
-      .reject { |new_pos| board[new_pos].color == color }
+    selected = potential_moves.select! { |move| board.valid_pos?([move[0], move[1]]) }
+    p selected
+    selected.reject { |new_pos| board[new_pos].color == color }
   end
 
   def move_diffs
